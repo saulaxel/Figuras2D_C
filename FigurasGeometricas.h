4a41af07fe9662d070bc4040b8f2c64cbd574f46
Saul Axel <saulaxel.50@gmail.com>
2017-12-09 16:54:50 -0600

    File list

--- FigurasGeometricas.h
+++ FigurasGeometricas.h
@@ -1,6 +1,11 @@
-#ifndef FIGURAS_GEOMETRICAS
-#define FIGURAS_GEOMETRICAS
+#ifndef FIGURAS_2D_H
+#define FIGURAS_2D_H
 
+#include <punto.h>
+#include <linea.h>
+#include <triangulo.h>
+#include <rectangulo.h>
+#include <circulo.h>
+#include <poligono.h>
 
-
-#endif // FIGURAS_GEOMETRICAS
+#endif // FIGURAS_2D_H
---
+++ punto.h
@@ -0,0 +1,9 @@
+#ifndef PUNTO_H
+#define PUNTO_H
+
+typedef _punto {
+    double x;
+    double y;
+} Punto;
+
+#endif /* PUNTO_H */
